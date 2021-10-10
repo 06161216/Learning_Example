90901ad2b1857d4ca0ec1457470833aafac76716
06161216 <ryogo.miyasaka.5c@stu.hosei.ac.jp>
2021-10-10 14:14:43 +0900

    [add]記事詳細画面追加ー

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,8 +9,13 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('index')->with(['posts' => $post->get()]);
+        return view('index')->with(['posts' => $post->getPagenateByLimit()]);
         
         //  return $post->get();
+    }
+
+    public function show(Post $post)
+    {
+        return view('show')->with(['post' => $post]);
     }
 }
