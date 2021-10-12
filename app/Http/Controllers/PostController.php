e8935ab1bd2c97f1bf0e5da3f3283932f3fe9057
06161216 <91449611+06161216@users.noreply.github.com>
2021-10-12 12:28:30 +0900

    Revert "記事一覧画面追加！"

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,25 +9,6 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('index')->with(['posts' => $post->getPagenateByLimit()]);
-        
-        //  return $post->get();
-    }
-
-    public function show(Post $post)
-    {
-        return view('show')->with(['post' => $post]);
-    }
-
-    public function create()
-    {
-        return view('create');
-    }
-
-    public function store(Post $post, Request $request)
-    {
-       $input = $request['post'];
-       $post->fill($input)->save();
-       return redirect('/posts/' . $post->id);
+        return $post->get();
     }
 }
